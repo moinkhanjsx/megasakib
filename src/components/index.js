740cdcec2654f5c70b78381cf782714ce1001f82
kmoin6231@gmail.com <kmoin6231@gmail.com>
2025-10-04 10:12:58 +0530

    Fix case-sensitive imports for Linux deployment

--- src/components/index.js
+++ src/components/index.js
@@ -1,17 +1,17 @@
 import Header from './header/header.jsx';
 import Footer from './footer/footer.jsx';
 import Container from './container/container.jsx'
-import Logo from './Logo.jsx';
+import Logo from './logo.jsx';
 import LogoutBtn from './header/logoutBtn.jsx';
 import RTE from './RTE.jsx';
-import Signup from './Signup.jsx';
-import Login from './Login.jsx';
-import Button from './Button.jsx';
+import Signup from './signup.jsx';
+import Login from './login.jsx';
+import Button from './button.jsx';
 import PostForm from './post-form/PostForm.jsx';
-import PostCard from './PostCard.jsx';
-import AuthLayout from './AuthLayout.jsx';
-import Input from './Input.jsx';
-import Select from './Select.jsx';
+import PostCard from './postCard.jsx';
+import AuthLayout from './authLayout.jsx';
+import Input from './input.jsx';
+import Select from './select.jsx';
 
 export{
     Header,
